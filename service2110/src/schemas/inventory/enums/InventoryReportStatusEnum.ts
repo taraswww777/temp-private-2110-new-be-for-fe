2765eacf825ee0bb63e9391742870008b51b0900
Александр Зинченко <aliaksandr.zinchanka@artezio.com>
2026-06-11 11:53:53 +0300

    updated inventory schemas

--- service2110/src/schemas/inventory/enums/InventoryReportStatusEnum.ts
+++ service2110/src/schemas/inventory/enums/InventoryReportStatusEnum.ts
@@ -5,6 +5,8 @@ export enum InventoryReportStatusEnum {
   IN_PROGRESS = 'IN_PROGRESS',
   COMPLETED = 'COMPLETED',
   FAILED  = 'FAILED',
+  CONTINUED = 'CONTINUED',
+  ROW_LIMIT_EXCEEDED = 'ROW_LIMIT_EXCEEDED',
 }
 
 export const inventoryReportStatusSchema = z
